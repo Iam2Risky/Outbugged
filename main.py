948a1995342ec632397f48c29d1e0b8e73bd90c1
Iam2Risky <IamnotRisky@gmail.com>
2025-01-24 09:35:49 +0100

    V0.1

--- main.py
+++ main.py
@@ -2,3 +2,27 @@
 Main Script für den Chatbot
 
 """
+
+import text
+
+bots = {"Bot 1": "Gardenbeetle", "Bot 2": "Cleanbug", "Bot 3": "Windowfly"}
+
+class main:
+    bot = bot("Debuggy")
+
+    print(bot.Startup())
+    
+
+class bot:
+    def __init__(self, name):
+        #Bot Variablen erstellen
+        self.name = name
+            
+    def Startup(self):
+        return(f"Hello my name is {self.name}.\nWhat kind of bot do you have problem with\n")
+    
+    def Response(self, userInput):
+        print("Work in progress")
+        
+
+
---
+++ text.py
@@ -0,0 +1,14 @@
+import re
+
+def TopicMatching(textInput):
+    synonyms = {
+        "charging": ["charge", "charging"],
+        "load": ["load", "loading"],
+        "break": ["break", "broken"],
+
+    }
+
+
+
+    
+
